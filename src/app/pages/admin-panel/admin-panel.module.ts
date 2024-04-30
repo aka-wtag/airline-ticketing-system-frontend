7dfd5ae8f8d3e3927c372375b0c0d525766e16d6
aka-wtag <ashraful.kabir@welldev.io>
2024-04-30 08:50:41 +0600

    code refactor

--- src/app/pages/admin-panel/admin-panel.module.ts
+++ src/app/pages/admin-panel/admin-panel.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AdminPanelComponent } from './admin-panel.component';
 import { HeaderComponent } from './header/header.component';
