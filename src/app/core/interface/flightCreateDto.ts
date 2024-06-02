1cc9567a9ed288be630b2a7089c97e540f5d27e1
aka-wtag <ashraful.kabir@welldev.io>
2024-06-02 18:28:03 +0600

    code refactor

---
+++ src/app/core/interface/flightCreateDto.ts
@@ -0,0 +1,12 @@
+export interface FlightCreateDTo {
+    arrivalDate: string;
+    arrivalLocation: string;
+    arrivalTime: string;
+    departureDate: string;
+    departureLocation: string;
+    departureTime: string;
+    fare: number;
+    flightId: number;
+    remainingSeats: number;
+    airlineId: number
+  }
